d1513b290e6aebd8d2985ac1298ded10137a62c2
Zhang Sheng <zerone216@163.com>
2019-11-26 21:55:40 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,45 @@
-/****************************************************************************************************************************
-*
-*   文 件 名 ： main.c 
-*   文件描述 ：  
-*   创建日期 ：2019年5月13日
-*   版本号 ：   
-*   负责人 ： Zhangsheng 
-*   其 他 ：
-*   修改日志 ：
-*
-****************************************************************************************************************************/
+/*
+ > Declaration: 
+ >  Copyright (c) 2009-2019 Zhang Sheng (Mr. Zhang) 
+ >  
+ >  This program is free software; you can redistribute it and/or modify it under 
+ >  the terms of the GNU General Public License as published by the Free Software 
+ >  Foundation, either version 2 or any later version.
+ >  
+ >  Redistribution and use in source and binary forms, with or without modification,
+ >  are permitted provided that the following conditions are met: 
+ >  
+ >  1. Redistributions of source code must retain the above copyright notice, this
+ >  list of conditions and the following disclaimer.
+ >  
+ >  2. Redistributions in binary form must reproduce the above copyright notice,
+ >  this list of conditions and the following disclaimer in the documentation and/or 
+ >  other materials provided with the distribution. 
+ > 
+ >  This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+ >  WARRANTY; without even the implied warranty of MERCHANTABILITY or ITNESS FOR A 
+ >  PARTICULAR PURPOSE. See the GNU General Public License for more details. A copy 
+ >  of the GNU General Public License is available at: 
+ >  http://www.fsf.org/licensing/licenses 
+ >  
+ >  All manufacturing, reproduction, use, and sales rights pertaining to this subject 
+ >  matter are governed bythe license agreement. The recipient of this software 
+ >  implicitly accepts the terms of the license.
+ > 
+ > Author: Zhang Sheng
+ > FilePath: \zerone216\BaseUtils\src\network\communication\tcp\libtcp.c
+ > CreateTime: 2019-11-26 21:06:57
+ > ModifyTime: 2019-11-26 21:13:52
+ > LastEditors: 
+ > Description: 
+ > version: 
+ > E-Mail: zerone216@163.com
+ > Homepage: http://www.zerone216.top:8080
+ > Repository: https://github.com/zerone216/
+ > Support: 
+ > Others: 
+ > Changelogs: 
+ */
 
 #include <stdio.h>
 #include <string.h>
